bc6b3515846569978c2d7947c55c126dc86cd010
NYN31 <noyoncse310197@gmail.com>
2024-03-11 10:52:44 +0600

    Solution of: 791 Custom sort string

---
+++ leetcode/0791-Custom-Sort-String.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  string myPush(string& curr, int& times, char& ch) {
+    for (int i = 1; i <= times; i++) curr.push_back(ch);
+    return curr;
+  }
+
+ public:
+  string customSortString(string order, string s) {
+    vector<int> frq(26, 0);
+    string curr = "";
+    for (auto& ch : s) frq[ch - 'a']++;
+    for (auto& ch : order) {
+      int times = frq[ch - 'a'];
+      if (times > 0) {
+        curr = myPush(curr, times, ch);
+        frq[ch - 'a'] = 0;
+      }
+    }
+    for (int i = 0; i < 26; i++) {
+      for (int j = 0; j < frq[i]; j++) {
+        curr.push_back(i + 'a');
+      }
+    }
+    return curr;
+  }
+};
