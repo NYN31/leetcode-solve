5083fce370252117212b1cb1acf9b2042cbbdfcf
NYN31 <noyoncse310197@gmail.com>
2024-07-04 11:33:31 +0600

    Solution of: 2181 - Merge nodes in between zeros

---
+++ leetcode/2181-Merge-Nodes-in-Between-Zeros.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* reverse(ListNode* head) {
+    ListNode *curr = head, *nxt = nullptr, *prev = nullptr;
+    while (curr) {
+      nxt = curr->next;
+      curr->next = prev;
+
+      prev = curr;
+      curr = nxt;
+    }
+    return prev;
+  }
+
+  ListNode* mergeNodes(ListNode* head) {
+    int sum = 0;
+    ListNode *temp = head->next, *ans = nullptr;
+    while (temp) {
+      sum += temp->val;
+      if (temp->val == 0) {
+        ans = new ListNode(sum, ans);
+        sum = 0;
+      }
+      temp = temp->next;
+    }
+
+    return reverse(ans);
+  }
+};
